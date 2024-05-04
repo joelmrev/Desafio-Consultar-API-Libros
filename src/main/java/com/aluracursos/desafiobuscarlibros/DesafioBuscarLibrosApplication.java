92c17a5e321ac9664342a1073fe8191596072cd3
joelmrev <joelmrev@gmail.com>
2024-05-04 00:54:54 -0600

    ADD: Inicio de Proyecto

---
+++ src/main/java/com/aluracursos/desafiobuscarlibros/DesafioBuscarLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiobuscarlibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBuscarLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBuscarLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/desafiobuscarlibros/DesafioBuscarLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiobuscarlibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBuscarLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
